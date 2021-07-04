89c3ccd181ea6224750e2e00f92b74811c03251b
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-04 17:15:49 +0900

    ContentViewを修正

--- misoranmen/ContentView.swift
+++ misoranmen/ContentView.swift
@@ -2,20 +2,49 @@
 //  ContentView.swift
 //  misoranmen
 //
-//  Created by 宮本光直 on 2021/07/04.
+//  Created by 宮本光直 on 2021/06/28.
 //
-
+import ComposableArchitecture
 import SwiftUI
 
 struct ContentView: View {
+    
+    let store: Store<ArticleState, ArticleAction>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            WithViewStore(store) { viewStore in
+                TabView {
+                    ArticleContentView(store: store)
+                        .tabItem {
+                            Label("article", systemImage: "gamecontroller")
+                        }
+                    
+                    ArticleFavoriteContentView(store: store)
+                        .tabItem {
+                            Label("favorite", systemImage: "star")
+                        }
+                }
+                .navigationBarTitle("Article List", displayMode: .inline)
+                .navigationBarItems(
+                    trailing: HStack {
+                        Button(action: {
+                            //
+                        }, label: {
+                            Image(systemName: "person.fill")
+                                .resizable()
+                                .frame(width: 22, height: 22)
+                                .padding()
+                        })
+                    })
+            }
+            
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}

8225c5728df678e7b417963bf278319c90f02812
miyamoto3270 <miyamoto3270@arsaga.jp>
2021-07-04 16:41:37 +0900

    Initial Commit

---
+++ misoranmen/misoranmenApp.swift
@@ -0,0 +1,17 @@
+//
+//  misoranmenApp.swift
+//  misoranmen
+//
+//  Created by 宮本光直 on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct misoranmenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

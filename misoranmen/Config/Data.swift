af1db1fc7c333dc3ddc28965e4f6ce4c6a57c61c
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-07-04 17:15:07 +0900

    config追加

---
+++ misoranmen/Config/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  misoranmen
+//
+//  Created by 宮本光直 on 2021/07/04.
+//
+
+import Foundation
+
+final class APIConst {
+    static let BASE_URL = "https://qiita.com/api/v2/items"
+}
+
